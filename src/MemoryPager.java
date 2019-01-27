46a9a43f934c6f8fb81ecfa44fc64bcd94aa0781
Elsie Lewis <elewis23@emich.edu>
2019-01-27 01:04:29 -0500

    Delete MemoryPager.java

--- src/MemoryPager.java
+++
@@ -1,9 +0,0 @@
-// Interface used for memory paging schemes
-public interface MemoryPager {
-	// Select a page to overwrite
-	public int pickPage();
-	// Bind a page table to the pager
-	public void bindPageTable(PageTable pageTable);
-	// Notify the pager that time has advanced
-	public void tick();
-}
